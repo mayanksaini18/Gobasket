b439aec61a318caa88526e8929bd249605c232e0
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-29 17:56:23 +0530

    some rafce in pages fo routing check

---
+++ grocery-web/src/pages/Checkout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Checkout = () => {
+  return (
+    <div>
+      checkout pge
+    </div>
+  )
+}
+
+export default Checkout
---
+++ grocery-web/src/pages/ProductDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <div>
+      products details
+    </div>
+  )
+}
+
+export default ProductDetails
