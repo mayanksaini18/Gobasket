68304af02a3a235db63ff0901d5fd8cdd886ad8e
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-30 18:39:49 +0530

    Update Register.tsx

--- grocery-web/src/pages/Register.tsx
+++ grocery-web/src/pages/Register.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>
-      Register page
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Register
+  const handleRegister = () => {
+    // TEMP: mock register success
+    navigate("/home");
+  };
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Create Account</h2>
+
+      <input placeholder="Name" />
+      <br />
+      <input placeholder="Email" />
+      <br />
+      <input placeholder="Password" type="password" />
+      <br />
+
+      <button onClick={handleRegister}>Sign Up</button>
+    </div>
+  );
+};
+
+export default Register;
