0ce4981a3cd4463776b284f42fa465708541a5e3
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-30 18:39:38 +0530

    Update App.tsx

--- grocery-web/src/App.tsx
+++ grocery-web/src/App.tsx
@@ -1,13 +1,12 @@
 import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
+
+
+
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <AppRoutes />
-      <Footer />
+    <>  
+    <AppRoutes/>
     </>
   );
 }
