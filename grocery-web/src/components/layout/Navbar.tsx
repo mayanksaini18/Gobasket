c4253d1e1432c4392d62d74eb09496b85d363244
Mayank Saini <mayanksaini0416@gmail.com>
2025-12-30 18:39:39 +0530

    Update Navbar.tsx

--- grocery-web/src/components/layout/Navbar.tsx
+++ grocery-web/src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav style={{ padding: "1rem", borderBottom: "1px solid #ddd" }}>
-      <Link to="/">Grocery</Link>{" "}
+      <Link to="/home">Home</Link>{" "}
       <Link to="/cart">Cart</Link>{" "}
       <Link to="/orders">Orders</Link>
     </nav>
